f4f6aa6d28e53aa6a48c4a21c65fe1c59b5c87e0
Kadenn <kadennathani@gmail.com>
2019-04-08 09:44:00 -0500

    j

--- src/Point.java
+++ src/Point.java
@@ -36,5 +36,39 @@ public class Point
         double varY = Math.pow(p1.y-p2.y, 2);
         return Math.sqrt(varX + varY);
     }
+    public boolean isVertical(Point other)
+    {
+        if (x == other.x)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+    public double slope(Point other)
+    {
+        return (double)(other.y-y)/(other.x-x);
+    }
+    public int manhattanDistance(Point other)
+    {
+        return (Math.abs(other.x-x) + (other.y - y));
+    }
+    public boolean isCollinear(Point p1, Point p2)
+    {
+        if (x == p1.x && x == p2.x)
+        {
+            return true;
+        }
+        else if ((double) Math.round(slope(p1)*10000)/10000 == (double) Math.round(slope(p2)*10000)/10000)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
 
 }
