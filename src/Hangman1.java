bf37c006d79323c3c1fe5474a8f7da1a9ebc3b6a
Kadenn <kadennathani@gmail.com>
2019-05-10 11:19:50 -0500

    j

--- src/Hangman1.java
+++ src/Hangman1.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Hangman1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please type the word");
         String word = sc.next();
+        String letterstoguess = "";
         int wordlength = word.length();
         int wordcount = 0;
         int guesscount = 0;
@@ -14,19 +16,39 @@ public class Hangman1 {
             System.out.print("_ ");
             wordcount++;
         }
+        System.out.println();
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> badlist = new ArrayList<String>();
         //Here is the loop of the player guessing letters.
-        while (guesscount<7)
+        while (guesscount<7 && !(word.equalsIgnoreCase(letterstoguess)))
         {
             System.out.println("Please guess a letter");
-            String letterguess = sc.next();
-            try
-            {
-                int try1 = word.indexOf(letterguess);
-                System.out.println(letterguess+" was in the word");
-            }
-            catch(NumberFormatException nfe){
+            String currentguess = sc.next();
+                int try1 = word.indexOf(currentguess);
+                if(try1<0 && !(badlist.contains(currentguess))){
+                    System.out.println(currentguess+" was not in the word");
+                    guesscount++;
+                    badlist.add(currentguess);
+                }
+                else if(try1>= 0 && !(list.contains(currentguess))){
+                    System.out.println(currentguess+" was in the word at index "+try1);
+                    list.add(currentguess);
+                    letterstoguess = letterstoguess+currentguess;
+                }
+                else if(try1>= 0 && list.contains(currentguess)) {
+                    System.out.println(currentguess + " has already been guessed and is in the word at index "+try1);
+                }
+                else if(try1<0 && badlist.contains(currentguess)){
+                    System.out.println(currentguess +" has already been guessed but is not in the word");
+                }
 
-            }
+        }
+        if(word.equalsIgnoreCase(letterstoguess)){
+            System.out.println("You have guessed the word");
+        }
+        else if(guesscount ==7)
+        {
+            System.out.println("You ran out of guesses. The man died.");
         }
 
 
